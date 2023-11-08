016e73e1199ddfe7896a549e4a129392b2443851
brij2700 <61970561+brij2700@users.noreply.github.com>
2023-11-08 10:28:56 -0500

    Time: 62 ms (72.21%), Space: 16.3 MB (33.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,29 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        carry=0
-        answer=ListNode()
-        head=answer
-        while(l1 or l2 or carry):
-            answer.next=ListNode()
-            answer=answer.next
-            l1_val=l1.val if l1 else 0
-            l2_val=l2.val if l2 else 0
-            l1=l1.next if l1 else None
-            l2=l2.next if l2 else None
-            sum_val=l1_val+l2_val+carry
-            carry=sum_val//10
-            sum_val=sum_val%10
-            answer.val=sum_val
-        return head.next
-
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+            result=ListNode()
+            cur=result
+            carry=0
+            while(l1 or l2 or carry):
+                l1val=l1.val if l1 else 0
+                l2val=l2.val if l2 else 0
+                numsum=l1val+l2val+carry
+                carry=numsum//10
+                numsum%=10
+                cur.next=ListNode(numsum)
+                cur=cur.next
+                l1=l1.next if l1 else None
+                l2=l2.next if l2 else None
+            return result.next
+                
+                
+        
             
+        
+            
+            
+            
+            
+                
+        
